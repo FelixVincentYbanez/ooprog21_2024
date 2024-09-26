b8620e3e70efc1a47ade69da8b83722dc241b494
FelixVincentYbañez <ybanezfelix22@gmail.com>
2024-09-26 21:51:25 +0800

    Create FirstAddressExcercise.java

---
+++ Chapter 3/FirstAddressExcercise.java
@@ -0,0 +1,12 @@
+public class FirstAddressExcercise{
+    public static void displayAddress(){
+        System.out.println("Samsung");
+        System.out.println("Suwon, Gyeonggi Province");
+        System.out.println("South Korea");
+        System.out.println("Company: Samsung Electronics Co., Ltd.");
+    }
+    
+    public static void main(String[] args){
+        displayAddress();
+    }
+}
